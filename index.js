dffcdb146c8e18ad669ab2fc28adf7d77e0d2cf1
Christofer Chan <devchumbing@gmail.com>
2018-05-22 00:36:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function theBeatlesPlay(x, y) {
+  let beatles = []
+  
+  for(var i = 0; i < x.length; i++){
+    beatles.push(x[i] + ' plays ' + y[i])
+  }
+  return beatles
+}
+
+function johnLennonFacts(x) {
+  var i = 0 
+  
+  while(i < x.length) {
+  x[i] += '!!!'
+   i++
+  }
+  return x
+}
+
+function iLoveBeatles(x){
+  var emptyArray = []
+  do {
+    amptyArray.push("I love beatles!")
+  } while(x < 15)
+  return emptyArray
+}
+
+
+
+
+
+
+
+
+
+
